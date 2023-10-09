58df92407f74794dc94d040774140983996132fc
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-10-09 19:06:37 +0800

    Fix installers

--- installserver.bat
+++ installserver.bat
@@ -1,5 +1,7 @@
+@echo off
+
 REM Install Node.js dependencies
-npm install express socket.io
+npm install express socket.io simple-peer-server
 
 REM Run the server
 node server.js
--- installserver.sh
+++ installserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Node.js dependencies
-npm install express socket.io
+npm install express socket.io simple-peer-server
 
 # Run the server
 node server.js
